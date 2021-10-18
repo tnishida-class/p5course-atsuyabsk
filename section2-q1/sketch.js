8731909b7cfbd62221049f006fd5f1f536c62772
atsuyabsk <91930665+atsuyabsk@users.noreply.github.com>
2021-10-18 22:39:13 +0900

    Change2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -2,8 +2,14 @@
 function setup(){
   createCanvas(100,100);
   for(let i = 0; i < 10; i++){
-    stroke(255,0,0)
-    ellipse(50, 50, 5 + i * 5);
-    // BLANK[1]
+    let x = 100 - i * 10;
+    if(i < 5){
+      stroke(255, 0, 0);
+      ellipse(50, 50, x);
+    }
+    else{
+      stroke(0, 0, 255);
+      ellipse(50, 50, x);
+    }
   }
 }
