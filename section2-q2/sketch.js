04226fbef4f5cda9344f516484ca51476274a4eb
atsuyabsk <91930665+atsuyabsk@users.noreply.github.com>
2021-10-18 23:19:47 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -3,9 +3,11 @@ function setup() {
   createCanvas(200, 200);
   let size = width / 8;
   noStroke();
+  fill(0, 255, 0);
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
+      rect(size *i, size * j, 10, 10);
     }
   }
 }
