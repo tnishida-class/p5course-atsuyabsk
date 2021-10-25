f386ad784e50e4046e8d7623737f0406f61961b4
atsuyabsk <91930665+atsuyabsk@users.noreply.github.com>
2021-10-25 15:38:55 +0900

    Change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -7,12 +7,23 @@ function setup() {
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
       if((i + j) % 2 == 1){
-        fill(22, 131, 45);
+        fill(0, 128, 128);
       }
       else{
         fill(255);
       }
-      rect(size *i, size * j, size, size);
+      rect(size * i, size * j, size, size);
+
+      if(j < 3 && (i + j) % 2 == 1){
+        fill(255, 0, 0);
+      }
+      else if (j < 5) {
+        noFill();
+      }
+      else if ((i + j) % 2 == 1){
+        fill(0, 0, 0);
+      }
+      circle(size *(i + 1 / 2), size * (j + 1 / 2), size * 4 / 5);
     }
   }
 }
