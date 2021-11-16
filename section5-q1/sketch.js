22691e5d382c20e2bc1ada4f20af2167eb6dc035
atsuyabsk <91930665+atsuyabsk@users.noreply.github.com>
2021-11-16 11:51:14 +0900

    Change section5-q1

--- section5-q1/sketch.js
+++ section5-q1/sketch.js
@@ -3,16 +3,22 @@
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
-  background(255);
-  balloon("I love keyakizaka46");
 }
 
-function balloon(t){
+function draw(){
+  background(255);
+  balloon("I love keyakizaka46" , 50, 50);
+}
+
+function balloon(t, x, y){
+  push();
   let w = textWidth(t);
   let h = textAscent() + textDescent();
   let p = 2;
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
+  rect(x, y, w + p * 2, h + p * 2);
+  triangle(x, y + h, x + 10, y + h, x, y + h + 10);
   fill(255);
-  text(t, p, h + p);
+  text(t, x + p, y + h + p);
+  pop();
 }
