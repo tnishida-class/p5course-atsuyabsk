59dc2b67473476f6d88ca5be5b5a916045efc0cf
atsuyabsk <91930665+atsuyabsk@users.noreply.github.com>
2021-11-26 14:02:24 +0900

    Change finalwork

--- finalwork/sketch.js
+++ finalwork/sketch.js
@@ -1,11 +1,51 @@
 // 最終課題を制作しよう
+let lineX;
+let lineY;
 
-function setup(){
+function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function draw(){
-  background(160, 192, 255);
+function draw() {
+  background(255);
+  wave();
+
+  if(mouseIsPressed){
+  human(width * 7 / 10, height * 1 / 10, height * 1 / 13);
+}
+
+  stroke(0);
+  textSize(50);
+  text("身体動揺", 10, 50);
+}
+
+function wave (){
+  for (let x = 0; x < width; x++){
+    let s = noise((x + frameCount) * 0.05);
+    let y = (height * 0.3 * s) + (height / 2);
+
+    stroke(0);
+    if (x > 0){
+      line(lineX, lineY, x , y);
+    }
+
+    lineX = x;
+    lineY = y;
+  }
+}
+
+function human (s, t, r){
+  push();
+  noFill();
+  stroke(0);
+  ellipse(s, t, r);
+  line(s, t + 5 / 2 * r, s - 1 / 2 * r, t + 5 * r);
+  line(s, t + 5 / 2 * r, s + 1 / 2 * r, t + 5 * r);
+  line(s - r, t + 1 / 2 * r, s - r, t + 5 / 2 * r);
+  line(s + r, t + 1 / 2 * r, s + r, t + 5 / 2 * r);
+  fill(50, 100, 200);
+  triangle(s - r, t + 1 / 2 * r, s + r, t + 1 / 2 * r, s, t + 5 / 2 * r);
+  pop();
 }
 
 function windowResized(){
